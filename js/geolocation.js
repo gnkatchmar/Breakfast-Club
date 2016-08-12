8b742e321a685defb66f95f2b6785c93d3b33d7b
djado <jdhabolt@djaango.com>
2016-08-12 16:34:49 -0700

    Google Places API is integrated!

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,12 +1,17 @@
 // GEOLOCATION API
 var x = document.getElementById('currentPosition');
 var pos;
-var map;
-var infoWindow;
+var mapObj;
+// var infoWindow;
 
 function initMap() {
-    infoWindow = new google.maps.InfoWindow({map: map});
-    var map = new google.maps.Map(document.getElementById('map'), {
+    // infoWindow = new google.maps.InfoWindow({map: map});
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //     center: {lat: 45.523832, lng: -122.676678},//initializes at city center
+    //     zoom: 13,
+    // });
+
+    mapObj = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 45.523832, lng: -122.676678},//initializes at city center
         zoom: 13,
     });
@@ -25,18 +30,18 @@ function initMap() {
     // });
 // TEST MARKERS
 
-
-    if (navigator.geolocation) {
+    if (navigator.geolocation){
       navigator.geolocation.getCurrentPosition(function(position) {
         pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
+
         console.log("Current Lat, Lng: " + pos.lat + ", " + pos.lng);
         infoWindow.setPosition(pos);//center view on user location
         infoWindow.setContent('Location found.');
         map.setCenter(pos);
-        infoWindow.open(map);//shows location. Not necessarily accurate
+        // infoWindow.open(map);//shows location. Not necessarily accurate
 
         var quad = getQuadrant();
         var brunchObjArr = getBrunchObjs(quad);
@@ -53,11 +58,11 @@ function initMap() {
         // USER MARKER
 
       }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
+        handleLocationError(true, infoWindow, mapObj.getCenter());
       });
     } else {
       // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+      handleLocationError(false, infoWindow, mapObj.getCenter());
     }
 }
 
---
+++ js/googlePlaces.js
@@ -0,0 +1,121 @@
+// Each array contains urls of images sized to the dimensions specified below
+// Image dimensions used to fill photoUrls arrays above
+// Changing these numbers will alter the urls in the photoUrls arrays
+    // Used to more easily fill the photoUrls arrays above
+var photosObj = {
+    global_pId: null,
+
+    // defining dimensions of images [sm] [md] [lg] [xl]
+    photoSizes: [100, 300, 500, 800],//sizes: sm, md, lg, xl
+
+    // container for image Url's of sizes [sm] [md] [lg] [xl]
+    // this array is attached to a brunchtracker when getPlacePhotos(pId) is called
+    // where pId is that object's pId property
+    photoUrlArrs: [[], [], [], []]
+};
+
+// Utility function
+function resetPhotosObj(){
+    photosObj.global_pId = null;
+    photosObj.photoUrlArrs = [[],[],[],[]];
+}
+
+// Utility function
+// Returns a tracker object by Google placeId
+function getTrackerObj(pId){
+    for(var i=0; i<brunchArr.length; i++){
+        if(brunchArr[i].pId === pId){
+            return brunchArr[i];
+        }
+    }
+}
+
+// Fill photoUrls arrays with images of brunchtracker with this pId
+function getPlacePhotos(pId){
+    if(!getTrackerObj(pId).hasOwnProperty('imgs')){
+        photosObj.global_pId = pId;
+        var request = {
+            placeId: pId
+        };
+        var service = new google.maps.places.PlacesService(mapObj);
+        service.getDetails(request, callback);
+    } else {
+        console.log("This tracker already has images! Exiting...");
+    }
+}
+
+// Fill the photUrls arrays
+function callback(place, status){
+    if (status == google.maps.places.PlacesServiceStatus.OK){
+        // console.log(place.name);
+        var photos = place.photos;
+        var url;
+
+        // for each photo size defined above
+        var photoSizes = photosObj.photoSizes;
+        for(var j=0; j<photoSizes.length; j++){
+            var size = photoSizes[j];
+
+            // get corresponding UrlArray
+            var urlArr = photosObj.photoUrlArrs[j];
+            // console.log("Filling array: " + size);
+
+            // fill that array with correctly sized image urls
+            for(var i=0; i<photos.length; i++){
+                url = photos[i].getUrl({'maxWidth':size, 'maxHeight':size});
+                urlArr.push(url);
+                // console.log(urlArr);
+            }
+        }
+        addPhotosToTracker(photosObj.global_pId);
+    }
+}
+
+function addPhotosToTracker(pId){
+    var brunchTrackerObj = getTrackerObj(pId);
+
+    if(!brunchTrackerObj.hasOwnProperty('imgs')){
+        brunchTrackerObj.imgs = [photosObj.photoUrlArrs[0].slice(), photosObj.photoUrlArrs[1].slice(), photosObj.photoUrlArrs[2].slice(), photosObj.photoUrlArrs[3].slice()];
+        console.log('Added imgs to tracker object!');
+        resetPhotosObj();
+
+        // Temporary just to demo
+        drawPhotos(getTrackerObj(pId),'container_googlePhotos', 'sm', 5);
+    } else {
+        console.log("This tracker already has images! Exiting...");
+    }
+}
+
+function drawPhotos(brunchtrackerObj, targetContainer, size, quantity){
+    console.log("Drawing photos for " + brunchtrackerObj.title);
+    var target = document.getElementById(targetContainer);
+    var activePhotoUrlArr;
+    target.innerHTML = "";
+
+    switch(size){
+        case 'sm':
+            activePhotoUrlArr = brunchtrackerObj.imgs[0];
+        break;
+
+        case 'md':
+            activePhotoUrlArr = brunchtrackerObj.imgs[1];
+        break;
+
+        case 'lg':
+            activePhotoUrlArr = brunchtrackerObj.imgs[2];
+        break;
+
+        case 'xl':
+            activePhotoUrlArr = brunchtrackerObj.imgs[3];
+        break;
+    }
+
+    if(quantity > activePhotoUrlArr.length){
+        quantity = activePhotoUrlArr.length;
+    }
+
+    for(var i=0; i<quantity; i++){
+        var url = activePhotoUrlArr[i];
+        target.innerHTML += "<img src='" + url + "'/>";
+    }
+}
