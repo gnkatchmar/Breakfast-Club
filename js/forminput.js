7337efbcbe309f000b9e243c91a1920fa4d89e23
gnkatchmar <gunk55@msn.com>
2016-08-17 14:16:54 -0700

    3rdPageFinal_smallHTMLfixes

--- js/forminput.js
+++ js/forminput.js
@@ -1,11 +1,14 @@
-function test (nh1, nh2, nh3) {
-  console.log(nh1.checked, nh2.checked, nh3.checked);
-  //returns boolean true/false for each of the threef
-  var e = document.getElementById("dropdown");
-  var timeChoice = e.options[e.selectedIndex].text;
-  console.log(timeChoice);
-  //returns time in same format as in brunch object
-}
+var reviewText = "";
+var rptContent = document.getElementById("review-content");
+
+//function test (nh1, nh2, nh3) {
+//  console.log(nh1.checked, nh2.checked, nh3.checked);
+//  //returns boolean true/false for each of the threef
+//  var e = document.getElementById("dropdown");
+//  var timeChoice = e.options[e.selectedIndex].text;
+//  console.log(timeChoice);
+//  //returns time in same format as in brunch object
+//}
 
 //time select listener
 document.getElementById("dropdown").onchange = function() {
@@ -44,44 +47,49 @@ if (localStorage.getItem("cb2")) {
   document.getElementById("cb2").checked = checked;
 }
 
-//// Event listener for first two review form elements unsure if needed
-//window.addEventListener('load', function(){
-//    var formEl_rselect = document.forms.restselect;
-//    console.log(formEl_rselect);
-//    document.getElementById('restselect').addEventListener('change', function(e){
-//
-//        // when radio buttons are changed,
-//        if(e.target.tagName === 'INPUT'){
-//            var vote = e.target.value;
-//            console.log(vote);
-//
-//        // when select is changed
-//        } else if(e.target.tagName === 'SELECT'){
-//            selectedRest = e.target.options[e.target.selectedIndex].text;
-//            console.log(selectedRest);
-//        }
-//    });
-//});
-
-function test2 (review) {
+function reviewPage (review) {
   var e = document.getElementById("restdropdown");
   var restChoice = e.options[e.selectedIndex].text;
-  console.log(restChoice);
-  console.log(review.value);
-  var f = document.getElementById("reviewtext").value;
-  console.log(f);
+  var restText = document.getElementById("reviewtext").value;
+    
+  //load or set local storage
+  if (localStorage.getItem("votes") == null) {
+    localStorage.setItem("votes",JSON.stringify(brunchArr));
+  } else {
+    brunchArr = JSON.parse(localStorage.getItem("votes"));
+  }
   
+   //helper function for text output
+  function reviewOutput () {
+    rptContent.innerHTML = "";
+    for (var i=0, iLen=brunchArr.length; i<iLen; i++) {
+      if (brunchArr[i].upvotes > 0 || brunchArr[i].downvotes > 0) {
+        reviewText = brunchArr[i].title + ": " + brunchArr[i].upvotes + " recommend and " + brunchArr[i].downvotes + " don't recommend. " + brunchArr[i].restcomment;
+        console.log(reviewText);
+        var rev = document.createElement("p");
+        rptContent.appendChild(rev);
+        var revtext = document.createTextNode(reviewText);
+        rev.appendChild(revtext);
+      } //if votes
+    } //for  
+  } 
+  
+  reviewOutput();
+  
+  //identify and update proper array member and local store
   for (var i=0, iLen=brunchArr.length; i<iLen; i++) {
     if (brunchArr[i].title === restChoice) {
       if (review.value === "recommend") {
         brunchArr[i].upvotes++;
-        console.log(brunchArr[i]);
+        brunchArr[i].restcomment += (' "' + restText + '"');
       } else if (review.value === "notrecommend") {
         brunchArr[i].downvotes++;
-        console.log(brunchArr[i]);
-      } //if/else if recommend 
+        brunchArr[i].restcomment += (' "' + restText + '"');
+      } //if/else if recommend
+      console.log(brunchArr[i]);
+      localStorage.setItem("votes",JSON.stringify(brunchArr));
     } //if title ==
-        
-    } //for
-}
+  } //for
   
+  reviewOutput(); 
+}
