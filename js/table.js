99c8d73e3aba49782473915f96e0bfc52101f8d3
Rosemary Joe <jjj430@msn.com>
2016-08-18 20:18:34 -0700

    clear table when time selected and add >= for times

--- js/table.js
+++ js/table.js
@@ -8,6 +8,9 @@ function myFunction() {
 }
 
 function getIndex() {
+	var tableContainer = document.getElementById("restaurantListTable");
+	tableContainer.innerHTML = "";
+	
     var selectDropDown = document.getElementById("dropdown");
 	var userSelectTime = selectDropDown.options[selectDropDown.selectedIndex].text;
 	console.log("this is a time selected "+ parseInt(userSelectTime));
@@ -41,7 +44,7 @@ function getIndex() {
 	// if SE is selected print out se restaurants
 	for (var index = 0; index < brunchArr.length; index++) {
 		//if (brunchArr[index].nhd == "SE" && parseInt(userSelectTime) > parseInt(brunchArr[index].opentime)) {
-		if (brunchArr[index].nhd == SEboxTrue && parseInt(userSelectTime) > parseInt(brunchArr[index].opentime)) {
+		if (brunchArr[index].nhd == SEboxTrue && parseInt(userSelectTime) >= parseInt(brunchArr[index].opentime)) {
 			var currentRestaurant = brunchArr[index];
 			currentRestaurant.addInfo();
 		}
@@ -50,7 +53,7 @@ function getIndex() {
 	// if NE is selected print out ne restaurants
 	for (var index = 0; index < brunchArr.length; index++) {
 		//if (brunchArr[index].nhd == "SE" && parseInt(userSelectTime) > parseInt(brunchArr[index].opentime)) {
-		if (brunchArr[index].nhd == NEboxTrue && parseInt(userSelectTime) > parseInt(brunchArr[index].opentime)) {
+		if (brunchArr[index].nhd == NEboxTrue && parseInt(userSelectTime) >= parseInt(brunchArr[index].opentime)) {
 			var currentRestaurant = brunchArr[index];
 			currentRestaurant.addInfo();
 		}
@@ -59,64 +62,13 @@ function getIndex() {
 	// if SW is selected print out sw restaurants
 	for (var index = 0; index < brunchArr.length; index++) {
 		//if (brunchArr[index].nhd == "SE" && parseInt(userSelectTime) > parseInt(brunchArr[index].opentime)) {
-		if (brunchArr[index].nhd == SWboxTrue && parseInt(userSelectTime) > parseInt(brunchArr[index].opentime)) {
+		if (brunchArr[index].nhd == SWboxTrue && parseInt(userSelectTime) >= parseInt(brunchArr[index].opentime)) {
 			var currentRestaurant = brunchArr[index];
 			currentRestaurant.addInfo();
 		}
-	}
-	
-	
-	
+	}	
 }
 
 
 
 
-//var e = document.getElementById("ddlViewBy");
-//var strUser = e.options[e.selectedIndex].text;
-
-/*
-for (var index = 0; index < brunchArr.length; index++) {
-	if (brunchArr[index].nhd == "SE" && brunchArr[index].opentime == "7:00 AM")	 {
-		var currentRestaurant = brunchArr[index];
-		currentRestaurant.addInfo();
-	}
-}
-*/
-
-/*
-for (var index = 0; index < brunchArr.length; index++) {
-	var currentRestaurant = brunchArr[index];
-	currentRestaurant.addInfo();
-}
-*/
-
-/*
-
-for (var index = 0; index < stores.length; index++) {
-  var currentStore = stores[index];
-  currentStore.cookieRandomHourlySales();
-  currentStore.addInfo();
-}
-*/
-/*
-this.addInfo = function() {
-     		var locationRow = document.createElement("tr");
-			var nameCell = document.createElement("td");//
-			nameCell.innerText = this.name;
-			locationRow.appendChild(nameCell);
-			
-			for (var i=0; i < 8; i++){
-				var cookieCell = document.createElement("td");
-				cookieCell.innerText = this.hourlyCookiesSales[i];
-				locationRow.appendChild(cookieCell);
-			}
-			
-			var totalCell = document.createElement("td");
-			totalCell.innerText = this.total;
-			locationRow.appendChild(totalCell);
-			
-			var table = document.getElementById("locations");
-			table.appendChild(locationRow);
-	};
-*/	
