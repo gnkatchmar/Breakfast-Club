2c1c13604363b75265eb1a3e127b869f71da7e91
djado <jdhabolt@djaango.com>
2016-08-20 00:59:28 -0700

    Added next3 transition out effect

--- js/geolocation.js
+++ js/geolocation.js
@@ -96,6 +96,16 @@ window.addEventListener('load', function(){
         enable_geoloc();
     });
     document.getElementById('btn_refresh').addEventListener('click', function(){
+        setTimeout(function(){
+            replaceClass('pic1', 'visible', 'invisible');
+        }, 50);
+        setTimeout(function(){
+            replaceClass('pic2', 'visible', 'invisible');
+        }, 100);
+        setTimeout(function(){
+            replaceClass('pic3', 'visible', 'invisible');
+        }, 150);
+
         getPlacePhotos();
     });
 });
